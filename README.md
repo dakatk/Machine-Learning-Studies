5b4a3f5ce40c545fbd33f52294f473e6f9473754
Dusten Knull <Semicolon@Dustens-MacBook-Pro.local>
2019-09-23 15:01:39 -0500

    Finished custom json_loader script

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Machine-Learning-Studies
+# Machine Learning Studies
+
+Goals:
+1. Create simple NNs in Python using libraries
+2. Assess performance and optimize these networks
+3. Create simple bare-bones in Python NNs that achieve the same results as previous NNs without using the libraries that were used before
+4. Translate the code from the previous step into three different languages:
+    a. Java (forced modularity, compiled to object code, cross-compatible)
+    b. Haskell (heavily optimized, compatible with C)
+    c. Javascript (web-based, lightweight, mix of Java and Python structure)
+5. Assess performance of each set of NNs based on the same tests run for the Python code
+6. Compare, assess why differences in performance are the way they are (if drastic differences do exist)
+7. Report on pros/cons/utilities of when/why each language should be used
--- json_loader.py
+++ json_loader.py
@@ -1,2 +1,50 @@
 #!/usr/bin/env python3
 
+from json import loads
+
+
+def get_data(json_file, ids):
+    """Load given 'ids' from segmented json objects in 'json_file'"""
+
+    values = dict()
+
+    with open(json_file, 'r') as f:
+        
+        for (i, line) in enumerate(f):
+
+            json_data = loads(line)
+            str_id = str(i + 1)
+            
+            values[str_id] = dict()
+
+            for jid in ids:
+                values[str_id][jid] = json_data[jid]
+                
+    return values
+
+# All review data can be connected by 'user_id' and 'business_id'
+# to chain information from all files together
+
+def get_restaurants_data():
+    """Load data of interest from 'restaurants.json' file"""
+    
+    return get_data('restaurants.json', ['price', 'stars', 'review_count', 'latitude', 'business_id'])
+
+
+def get_reviews_data():
+    """Load data of interest from 'reviews.json' file"""
+
+    return get_data('reviews.json', ['stars', 'text', 'business_id', 'user_id'])
+
+
+def get_users_data():
+    """Load data of interest from 'users.json' file"""
+
+    return get_data('users.json', ['review_count', 'average_stars', 'user_id'])
+
+
+if __name__ == '__main__':
+    
+    print(get_restaurants_data())
+    print(get_reviews_data())
+    print(get_users_data())
